66b39f6c7fbea313358e9fde7d097fe4da251bb2
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2024-01-29 23:17:17 +0600

    registration method

--- resources/js/components/register.vue
+++ resources/js/components/register.vue
@@ -9,26 +9,26 @@
 
                                 <div class="form-group mb-3">
                                     <label for="name">Name</label>
-                                    <input type="text" id="name" class="form-control mt-2" placeholder="Enter Your EMail" required>
+                                    <input type="text" v-model="name" id="name" class="form-control mt-2" placeholder="Enter Your EMail" required>
                                 </div>
 
                                 <div class="form-group mb-3">
                                     <label for="email">Email</label>
-                                    <input type="email" id="email" class="form-control mt-2" placeholder="Enter Your EMail" required>
+                                    <input type="email" v-model="email" id="email" class="form-control mt-2" placeholder="Enter Your EMail" required>
                                 </div>
 
                                 <div class="form-group mb-3">
                                     <label for="password">Password</label>
-                                    <input type="password" id="password" class="form-control mt-2" placeholder="Enter Your Password" required>
+                                    <input type="password" v-model="password" id="password" class="form-control mt-2" placeholder="Enter Your Password" required>
                                 </div>
 
                                 <div class="form-group mb-3">
-                                    <label for="password-confirm">Password</label>
-                                    <input type="password-confirm" id="password" class="form-control mt-2" placeholder="Enter Your Password" required>
+                                    <label for="password_confirm">Confirm Password</label>
+                                    <input type="password" v-model="confirm_password" id="confirm_password" class="form-control mt-2" placeholder="Confirm Password" required>
                                 </div>
 
                                 <div class="form-group">
-                                    <button class="btn btn-success">Register</button>
+                                    <button class="btn btn-success" @click.prevent="submitReg">Register</button>
                                 </div>
                             </form>
                     
@@ -40,9 +40,34 @@
 </template>
 
 <script>
+import axios from 'axios';
+
     export default {
+        name:"Register",
         mounted() {
             console.log('Component mounted.')
+        },
+        data(){
+            return{
+                name:'',
+                email:'',
+                password:'',
+                confirm_password:'',
+            }
+        },
+        methods:{
+            submitReg(){
+                axios.post('/register',{
+                    name:this.name,
+                    email:this.email,
+                    password:this.password,
+                    confirm_password:this.confirm_password,
+                }).then((response)=>{
+                    console.log(response,'response')
+                }).catch((error)=>{
+                    console.log('error')
+                })
+            }
         }
     }
 </script>
